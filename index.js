be166abfb9792fe98ebc9772d838e5de051fe340
Jan Brummelte <github@jan-brummelte.de>
2017-09-03 13:42:06 +0200

    Refactor config

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 module.exports = {
     parser: 'babel-eslint',
-    extends: [
-        'airbnb',
-        'plugin:ava/recommended',
-    ],
-    plugins: [
-        'import',
-        'ava',
-    ],
+    extends: ['airbnb', 'plugin:ava/recommended'],
+    plugins: ['import', 'ava'],
     rules: {
-        indent: ['error', 4, {
-            SwitchCase: 1,
-            MemberExpression: 1,
-            VariableDeclarator: 1,
-            outerIIFEBody: 1,
-            FunctionDeclaration: {
-                parameters: 1,
-                body: 1,
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+                MemberExpression: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+                FunctionDeclaration: { parameters: 1, body: 1 },
+                FunctionExpression: { parameters: 1, body: 1 },
             },
-            FunctionExpression: {
-                parameters: 1,
-                body: 1,
-            },
-        }],
+        ],
         strict: 'error',
         'no-sync': 'error',
         'no-inline-comments': 'error',
         'no-use-before-define': ['error', 'nofunc'],
         'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-        'max-len': ['error', 150, 4, {
-            ignoreUrls: true,
-            ignoreComments: false,
-            ignoreRegExpLiterals: true,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-        }],
-        'import/order': ['error', {
-            groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
-        }],
+        'max-len': [
+            'error',
+            150,
+            4,
+            { ignoreUrls: true, ignoreComments: false, ignoreRegExpLiterals: true, ignoreStrings: true, ignoreTemplateLiterals: true },
+        ],
+        'import/order': ['error', { groups: ['builtin', 'external', 'parent', 'sibling', 'index'] }],
         'import/named': 'error',
         'import/default': 'error',
         'import/namespace': 'error',
